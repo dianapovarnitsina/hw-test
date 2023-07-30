0c6ec9b72222c0152ba862867072c5aadb66dec8
Diana Povarnitsina <diana.povarnitsina@iqoption.com>
2023-07-30 14:11:40 +0300

    hw06_pipeline_execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,29 @@ type (
 
 type Stage func(in In) (out Out)
 
+func worker(done In, out Out, stage Stage) Out {
+	ch := make(Bi)
+	go func() {
+		defer close(ch)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-out:
+				if !ok {
+					return
+				}
+				ch <- val
+			}
+		}
+	}()
+	return stage(ch)
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = worker(done, out, stage)
+	}
+	return out
 }

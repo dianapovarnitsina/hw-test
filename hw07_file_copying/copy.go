508efa9fd780c881dab58d94a38d531aeccd55c5
Diana Povarnitsina <diana.povarnitsina@iqoption.com>
2023-08-09 23:07:21 +0300

    hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +14,51 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return fmt.Errorf("could not get stat from file: %w", err)
+	}
+	fileSize := fileInfo.Size()
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	fileFrom, err := os.Open(fromPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("error file does not exist: %w", err)
+		}
+		return fmt.Errorf("error open from file: %w", err)
+	}
+	defer fileFrom.Close()
+
+	_, err = fileFrom.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("error seeking to offset: %w", err)
+	}
+
+	var bufSize int64
+	if limit+offset > fileSize {
+		bufSize = fileSize - offset
+	} else if limit+offset <= fileSize && limit > 0 {
+		bufSize = limit
+	} else if limit == 0 {
+		bufSize = fileSize - offset
+	}
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("error creating destination file: %w", err)
+	}
+	defer fileTo.Close()
+	bar := pb.Full.Start64(fileSize)
+
+	_, err = io.Copy(fileTo, bar.NewProxyReader(io.LimitReader(fileFrom, bufSize)))
+	if err != nil {
+		return fmt.Errorf("error copying data: %w", err)
+	}
+	bar.Finish()
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,68 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+const (
+	fromPath   = "testdata/input.txt"
+	toPath     = "testdata/tmp/output.txt"
+	checkPath  = "testdata/out_offset6000_limit1000.txt"
+	offsetTest = 6000
+	limitTest  = 1000
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+
+	err := Copy(fromPath, toPath, offsetTest, limitTest)
+	if err != nil {
+		return
+	}
+
+	checkFile, err := os.Open(checkPath)
+	if err != nil {
+		return
+	}
+	defer checkFile.Close()
+	statCheck, err := checkFile.Stat()
+	if err != nil {
+		return
+	}
+
+	toFile, err := os.Open(toPath)
+	if err != nil {
+		return
+	}
+	defer toFile.Close()
+	statTo, err := toFile.Stat()
+	if err != nil {
+		return
+	}
+
+	require.Equal(t, statCheck.Size(), statTo.Size())
+
+	bufCheck := make([]byte, 1024)
+	_, err = checkFile.Read(bufCheck)
+	if err != nil {
+		return
+	}
+
+	bufTo := make([]byte, 1024)
+	_, err = toFile.Read(bufTo)
+	if err != nil {
+		return
+	}
+
+	require.Equal(t, bufCheck, bufTo)
+
+	cmd := exec.Command("rm", "-f", toPath)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }

10a1dc61a9cf381baa61b7931c73005d7a9d48df
Diana Povarnitsina <diana.povarnitsina@iqoption.com>
2023-09-10 11:41:43 +0300

    hw09_struct_validator

--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,24 +2,23 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
 )
 
 type UserRole string
 
-// Test the function on different structures and other types.
 type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
 		Name   string
-		Age    int             `validate:"min:18|max:50"`
-		Email  string          `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
-		Role   UserRole        `validate:"in:admin,stuff"`
-		Phones []string        `validate:"len:11"`
-		meta   json.RawMessage //nolint:unused
+		Age    int      `validate:"min:18|max:50"`
+		Email  string   `validate:"regexp:^\\w+@\\w+\\.\\w+$"`
+		Role   UserRole `validate:"in:admin,stuff"`
+		Phones []string `validate:"len:11"`
+		meta   json.RawMessage
 	}
-
 	App struct {
 		Version string `validate:"len:5"`
 	}
@@ -42,19 +41,66 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			1,
+			ErrNotStruct,
 		},
-		// ...
-		// Place your code here.
+		{
+			App{
+				Version: "12345",
+			},
+			ValidationErrors{},
+		},
+		{
+			Token{
+				Header:    []byte{1, 2},
+				Payload:   []byte{3, 4},
+				Signature: []byte{5, 6},
+			},
+			ValidationErrors{},
+		},
+		{
+			Response{
+				Code: 200,
+				Body: "",
+			},
+			ValidationErrors{},
+		},
+		{
+			User{
+				ID:     "1",
+				Name:   "test",
+				Age:    1,
+				Email:  "testtest.ru",
+				Role:   "admin",
+				Phones: []string{"1111111111"},
+				meta:   json.RawMessage(""),
+			},
+			ValidationErrors{
+				ValidationError{"ID", errLenString},
+				ValidationError{"Age", errValueIsLessThanMinValue},
+				ValidationError{"Email", errRegexpString},
+				ValidationError{"Phones", errLenString},
+			},
+		},
 	}
-
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
 			tt := tt
 			t.Parallel()
+			err := Validate(tt.in)
+			if validationErr, ok := err.(ValidationErrors); ok {
+				for _, e := range validationErr {
+					if e.Field == "Code" && !errors.Is(e.Err, tt.expectedErr) {
+						t.Errorf("Error: Expected: %v, but received: %v", tt.expectedErr, e.Err)
+					}
+				}
+			} else {
+				// Обработать случай, когда err - это одиночная ошибка, а не ValidationErrors.
+				if !errors.Is(err, tt.expectedErr) {
+					t.Errorf("Error: Expected: %v, but received: %v", tt.expectedErr, err)
+				}
+			}
 
-			// Place your code here.
-			_ = tt
 		})
 	}
 }

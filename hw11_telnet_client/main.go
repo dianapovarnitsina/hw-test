4ee3833aecf64d2583810e88eb5599f71fce1e5a
Diana Povarnitsina <diana.povarnitsina@iqoption.com>
2023-09-12 19:34:02 +0300

    hw11_telnet_client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,65 @@
 package main
 
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	timeout time.Duration
+)
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "connection timeout to host")
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	if len(flag.Args()) < 2 {
+		log.Fatal("program syntax: go-telnet <host> <port>")
+	}
+
+	address := net.JoinHostPort(flag.Args()[0], flag.Args()[1])
+	cl := NewTelnetClient(address, timeout, ioutil.NopCloser(os.Stdin), os.Stdout)
+
+	log.Printf("...Connected to %s\n", address)
+
+	if err := cl.Connect(); err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := cl.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for {
+			if err := cl.Send(); err != nil {
+				log.Fatal(err)
+				return
+			}
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for {
+			if err := cl.Receive(); err != nil {
+				log.Fatal(err)
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
 }
